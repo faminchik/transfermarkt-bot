19530538d7318458a43aaf8b9f1b1a73a1f7028f
Faminchik Evgeniy <f.zhek@mail.ru>
2019-03-10 15:23:22 +0300

    implement sending messages for TeamTrsanfers
    1. ability to opt the team (with keyboard buttons)
    2. team transfers (arrivals and departures)

--- src/db/helpers/clubCollectionHelpers.js
+++ src/db/helpers/clubCollectionHelpers.js
@@ -1,7 +1,4 @@
 import _ from 'lodash';
 import Club from 'models/Club';
 
-export const getClubLink = async clubName => {
-    const club = await Club.findOne({ clubName });
-    return _.get(club, 'clubLink', '');
-};
+export const getClubInfo = async clubName => await Club.findOne({ clubName });
--- src/helpers/telegram/telegramBotHelpers.js
+++ src/helpers/telegram/telegramBotHelpers.js
@@ -1,18 +1,32 @@
 import _ from 'lodash';
 import BPromise from 'bluebird';
-import { formTransferMessage, joinTransferMessages } from 'helpers/telegram/formMessageHelper';
+import {
+    formTransferMessage,
+    formTeamTransferMessage,
+    formTeamTransferHeader,
+    joinMessages,
+    formClubsSearchResultMessage
+} from 'helpers/telegram/formMessageHelper';
+import { formClubsSearchResultButtons } from 'helpers/telegram/formButtonsForKeyboard';
+import { formInlineKeyboard } from 'helpers/telegram/formKeyboards';
 import { BLOCKED, ERROR, SUCCESS } from 'constants/statuses';
 
-const sendMessage = async (botClient, chatId, message) =>
+const sendMessage = async (botClient, chatId, message, options = {}) =>
     await botClient
         .sendMessage(
             // '@transfers_transfermarkt',
             chatId,
             message,
-            { parse_mode: 'Markdown' }
+            { parse_mode: 'Markdown', ...options }
         )
         .then(res => SUCCESS)
         .catch(err => (err.response && err.response.statusCode === 403 ? BLOCKED : ERROR));
+
+const joinAndSendMessages = async (botClient, chatId, messages, header) => {
+    const joinedMessages = joinMessages(messages, header);
+
+    await BPromise.each(joinedMessages, async msg => await sendMessage(botClient, chatId, msg));
+};
 
 export const sendTransferMessage = async (botClient, chatId, transferInfo, isNewTransfer) => {
     const message = formTransferMessage(transferInfo, isNewTransfer);
@@ -23,7 +37,31 @@ export const sendTransferMessage = async (botClient, chatId, transferInfo, isNew
 export const sendJoinedTransferMessages = async (botClient, chatId, transfers) => {
     const transfersMessages = _.map(transfers, transferInfo => formTransferMessage(transferInfo));
 
-    const messages = joinTransferMessages(transfersMessages);
+    await joinAndSendMessages(botClient, chatId, transfersMessages);
+};
 
-    await BPromise.each(messages, async message => await sendMessage(botClient, chatId, message));
+// TODO make this method more general (not only for clubs)
+export const sendClubsSearchResultWithOptions = async (botClient, chatId, searchResult) => {
+    const message = formClubsSearchResultMessage(searchResult);
+    const keyboardButtons = formClubsSearchResultButtons(searchResult);
+
+    const options = { ...formInlineKeyboard(keyboardButtons) };
+    await sendMessage(botClient, chatId, message, options);
+};
+
+export const sendTeamTransfersMessages = async (botClient, chatId, { teamTransfers, clubInfo }) => {
+    const { teamTransfersArrivals, teamTransfersDepartures } = teamTransfers;
+
+    const arrivalsMessages = _.map(teamTransfersArrivals, (info, index) =>
+        formTeamTransferMessage(info, '← ', index + 1)
+    );
+    const arrivalsHeader = formTeamTransferHeader(clubInfo, 'Arrivals');
+
+    const departuresMessages = _.map(teamTransfersDepartures, (info, index) =>
+        formTeamTransferMessage(info, '→ ', index + 1)
+    );
+    const departuresHeader = formTeamTransferHeader(clubInfo, 'Departures');
+
+    await joinAndSendMessages(botClient, chatId, arrivalsMessages, arrivalsHeader);
+    await joinAndSendMessages(botClient, chatId, departuresMessages, departuresHeader);
 };
