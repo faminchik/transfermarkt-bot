55fd469f76ee907108199d4b13e4a388f3cdf01b
Evgeniy <f.zhek@mail.ru>
2020-04-04 21:44:22 +0300

    delete messages on team transfers

---
+++ src/helpers/telegram/apiHelpers.ts
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import TelegramBot, { SendMessageOptions, Chat, Message } from 'node-telegram-bot-api';
+import Status from 'constants/Statuses';
+
+/* eslint @typescript-eslint/camelcase: 0 */
+
+export const sendMessage = (
+    botClient: TelegramBot,
+    chatId: Chat['id'],
+    message: string,
+    options: SendMessageOptions = {}
+): Promise<Partial<Message> & { status: Status }> =>
+    botClient
+        .sendMessage(
+            // '@transfers_transfermarkt',
+            chatId,
+            message,
+            { parse_mode: 'Markdown', ...options }
+        )
+        .then(messageData => ({ ...messageData, status: Status.SUCCESS }))
+        .catch(err =>
+            err.response && err.response.statusCode === 403 ? { status: Status.BLOCKED } : { status: Status.ERROR }
+        );
+
+export const deleteMessage = (botClient: TelegramBot, chatId: Chat['id'], messageId: Message['message_id']) =>
+    botClient.deleteMessage(chatId, _.toString(messageId)).catch(() => false);
