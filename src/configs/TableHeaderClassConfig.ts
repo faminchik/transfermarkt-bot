7b547d94f5e45443d8ccd94cbe4111cd8aa8d06b
Evgeniy Faminchik <f.zhek@mail.ru>
2022-01-01 15:00:18 +0300

    fix main functionality

---
+++ src/configs/TableHeaderClassConfig.ts
@@ -0,0 +1,11 @@
+import pt from 'constants/transfermarkt/ParsingTypes';
+
+const TABLE_HEADER = '.table-header';
+const CONTENT_BOX_HEADLINE = '.content-box-headline';
+
+export default {
+    [pt.ALL_LATEST_TRANSFERS]: CONTENT_BOX_HEADLINE,
+    [pt.SEARCH_CLUBS]: TABLE_HEADER,
+    [pt.TEAM_TRANSFERS_ARRIVALS]: CONTENT_BOX_HEADLINE,
+    [pt.TEAM_TRANSFERS_DEPARTURES]: CONTENT_BOX_HEADLINE
+};
