a113d5156cc053b58e4ba0911e4fa457439b7434
Evgeniy Faminchik <f.zhek@mail.ru>
2023-01-08 12:18:18 +0100

    fix teams and players search

--- src/configs/TableHeaderClassConfig.ts
+++ src/configs/TableHeaderClassConfig.ts
@@ -1,12 +1,12 @@
 import pt from 'constants/transfermarkt/ParsingTypes';
 
-const TABLE_HEADER = '.table-header';
+// const TABLE_HEADER = '.table-header';
 const CONTENT_BOX_HEADLINE = '.content-box-headline';
 
 export default {
     [pt.ALL_LATEST_TRANSFERS]: CONTENT_BOX_HEADLINE,
-    [pt.SEARCH_CLUBS]: TABLE_HEADER,
-    [pt.SEARCH_PLAYERS]: TABLE_HEADER,
+    [pt.SEARCH_CLUBS]: CONTENT_BOX_HEADLINE,
+    [pt.SEARCH_PLAYERS]: CONTENT_BOX_HEADLINE,
     [pt.TEAM_TRANSFERS_ARRIVALS]: CONTENT_BOX_HEADLINE,
     [pt.TEAM_TRANSFERS_DEPARTURES]: CONTENT_BOX_HEADLINE,
     [pt.PLAYER_TRANSFERS_HISTORY]: CONTENT_BOX_HEADLINE
